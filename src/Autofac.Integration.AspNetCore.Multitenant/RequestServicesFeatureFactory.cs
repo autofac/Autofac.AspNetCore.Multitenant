fa3c04887e76ab472cb3a2e14ee5e42a4b4a5f0b
alsami <sami.alkhatib@zuehlke.com>
2022-06-07 17:23:43 +0200

    feat: simplify RequestServicesFeatureFactory since we will now always only have two parameters instead of one

--- src/Autofac.Integration.AspNetCore.Multitenant/RequestServicesFeatureFactory.cs
+++ src/Autofac.Integration.AspNetCore.Multitenant/RequestServicesFeatureFactory.cs
@@ -1,16 +1,9 @@
 ﻿// Copyright (c) Autofac Project. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System;
-using System.Linq;
-using System.Linq.Expressions;
-using Autofac.Integration.AspNetCore.Multitenant.Properties;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.DependencyInjection;
-#if NETSTANDARD2_0
-using Microsoft.AspNetCore.Hosting.Internal;
-#endif
 
 namespace Autofac.Integration.AspNetCore.Multitenant
 {
@@ -19,7 +12,7 @@ namespace Autofac.Integration.AspNetCore.Multitenant
     /// </summary>
     internal static class RequestServicesFeatureFactory
     {
-        private static readonly Lazy<Func<HttpContext, IServiceScopeFactory, IServiceProvidersFeature>> FactoryFunction = new Lazy<Func<HttpContext, IServiceScopeFactory, IServiceProvidersFeature>>(GenerateLambda);
+        // private static readonly Lazy<Func<HttpContext, IServiceScopeFactory, IServiceProvidersFeature>> FactoryFunction = new Lazy<Func<HttpContext, IServiceScopeFactory, IServiceProvidersFeature>>(GenerateLambda);
 
         /// <summary>
         /// The function that creates the <see cref="IServiceProvidersFeature"/> based on the framework version.
@@ -29,43 +22,7 @@ namespace Autofac.Integration.AspNetCore.Multitenant
         /// <returns>An instance of <see cref="IServiceProvidersFeature"/>.</returns>
         public static IServiceProvidersFeature CreateFeature(HttpContext context, IServiceScopeFactory scopeFactory)
         {
-            return FactoryFunction.Value.Invoke(context, scopeFactory);
-        }
-
-        private static Func<HttpContext, IServiceScopeFactory, IServiceProvidersFeature> GenerateLambda()
-        {
-            var constructor = typeof(RequestServicesFeature).GetConstructors().OrderByDescending(ci => ci.GetParameters().Length).First();
-            var parameterCount = constructor.GetParameters().Length;
-            var contextParameter = Expression.Parameter(typeof(HttpContext), "context");
-            var factoryParameter = Expression.Parameter(typeof(IServiceScopeFactory), "factory");
-            switch (parameterCount)
-            {
-                case 1:
-                    // ASP.NET Core through 2.1 has RequestServicesFeature ctor only with scope factory.
-                    var subExpression = Expression.Lambda<Func<IServiceScopeFactory, IServiceProvidersFeature>>(
-                        Expression.Convert(
-                            Expression.New(
-                                constructor,
-                                factoryParameter),
-                            typeof(IServiceProvidersFeature)),
-                        factoryParameter)
-                        .Compile();
-                    return (ctx, factory) => subExpression(factory);
-                case 2:
-                    // ASP.NET Core 2.1 changed RequestServicesFeature ctor to take context and scope factory.
-                    return Expression.Lambda<Func<HttpContext, IServiceScopeFactory, IServiceProvidersFeature>>(
-                        Expression.Convert(
-                            Expression.New(
-                                constructor,
-                                contextParameter,
-                                factoryParameter),
-                            typeof(IServiceProvidersFeature)),
-                        contextParameter,
-                        factoryParameter)
-                        .Compile();
-                default:
-                    throw new NotSupportedException(Resources.NoSupportedRequestServicesConstructorFound);
-            }
+            return new RequestServicesFeature(context, scopeFactory);
         }
     }
 }
