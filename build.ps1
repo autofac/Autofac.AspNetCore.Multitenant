44b5bc26068b163b19a20cae6333e177ed534a34
Travis Illig <tillig@paraesthesia.com>
2022-06-30 11:15:00 -0700

    Updated analyzers, build, packaging for NET6.
    Includes updates to better support VS Code - .NET TFM needs to be
    ordered in descending order (VS Code uses the first one listed).
    
    Most analyzers are built into the .NET 6 build, no need for potentially
    conflicting analyzers in other TFMs.
    
    Rulesets, .editorconfig, .csproj all brought into alignment with core
    Autofac.

--- build.ps1
+++ build.ps1
@@ -19,12 +19,17 @@ try {
         Remove-Item $artifactsPath -Force -Recurse
     }
 
-    # Install dotnet CLI
-    Install-DotNetCli -Version $sdkVersion
-
-    foreach ($additional in $globalJson.additionalSdks)
-    {
-        Install-DotNetCli -Version $additional;
+    # Install dotnet SDK versions during CI. In a local build we assume you have
+    # everything installed; on CI we'll force the install. If you install _any_
+    # SDKs, you have to install _all_ of them because you can't install SDKs in
+    # two different locations. dotnet CLI locates SDKs relative to the
+    # executable.
+    if ($Null -ne $env:APPVEYOR_BUILD_NUMBER) {
+        Install-DotNetCli -Version $sdkVersion
+        foreach ($additional in $globalJson.additionalSdks)
+        {
+            Install-DotNetCli -Version $additional;
+        }
     }
 
     # Write out dotnet information
@@ -33,7 +38,7 @@ try {
     # Set version suffix
     $branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$NULL -ne $env:APPVEYOR_REPO_BRANCH];
     $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$NULL -ne $env:APPVEYOR_BUILD_NUMBER];
-    $versionSuffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)).Replace('/', '-'))-$revision" }[$branch -eq "master" -and $revision -ne "local"]
+    $versionSuffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)).Replace('/', '-').Replace('_', '-'))-$revision" }[$branch -eq "master" -and $revision -ne "local"]
 
     Write-Message "Package version suffix is '$versionSuffix'"
 
@@ -51,9 +56,19 @@ try {
 
     if ($env:CI -eq "true") {
         # Generate Coverage Report
+        Write-Message "Downloading and verifying Codecov Uploader"
+        $ProgressPreference = 'SilentlyContinue'
+        Invoke-WebRequest -Uri https://keybase.io/codecovsecurity/pgp_keys.asc -OutFile codecov.asc
+        gpg --no-default-keyring --keyring trustedkeys.gpg --import codecov.asc
+        Invoke-WebRequest -Uri https://uploader.codecov.io/latest/linux/codecov -Outfile codecov
+        Invoke-WebRequest -Uri https://uploader.codecov.io/latest/linux/codecov.SHA256SUM -Outfile codecov.SHA256SUM
+        Invoke-WebRequest -Uri https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig -Outfile codecov.SHA256SUM.sig
+        gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
+        shasum -a 256 -c codecov.SHA256SUM
+        chmod +x codecov
+
         Write-Message "Generating Codecov Report"
-        Invoke-WebRequest -Uri 'https://codecov.io/bash' -OutFile codecov.sh
-        & bash codecov.sh -f "artifacts/coverage/*/coverage*.info"
+        & ./codecov -f "artifacts/coverage/*/coverage*.info"
     }
 
     # Finished
